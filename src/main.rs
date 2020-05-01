c05b7e42db297eb8b81d40f09ef15d3b135cd9ec
Qishen Zhang <qishen23@gmail.com>
2020-05-01 02:01:55 -0500

    Add an ugly commandline interface.

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,99 @@
+extern crate differential_formula;
 extern crate clap;
-use clap::{Arg, App, SubCommand};
+
+use std::fs;
+use std::io;
+use std::io::Write;
+
+use clap::{Arg, ArgMatches, App, SubCommand};
+
+use differential_formula::term::*;
+use differential_formula::engine::*;
+use differential_formula::module::*;
+use differential_formula::type_system::*;
 
 fn main() {
-    let matches = App::new("My Super Program")
-                          .version("1.0")
-                          .author("Kevin K. <kbknapp@gmail.com>")
-                          .about("Does awesome things")
-                          .arg(Arg::with_name("config")
-                               .short("c")
-                               .long("config")
-                               .value_name("FILE")
-                               .help("Sets a custom config file")
-                               .takes_value(true))
-                          .arg(Arg::with_name("INPUT")
-                               .help("Sets the input file to use")
-                               .required(true)
-                               .index(1))
-                          .arg(Arg::with_name("v")
-                               .short("v")
-                               .multiple(true)
-                               .help("Sets the level of verbosity"))
-                          .subcommand(SubCommand::with_name("test")
-                                      .about("controls testing features")
-                                      .version("1.3")
-                                      .author("Someone E. <someone_else@other.com>")
-                                      .arg(Arg::with_name("debug")
-                                          .short("d")
-                                          .help("print debug information verbosely")))
-                          .get_matches();
+    
+    let mut engine = DDEngine::new();
+    engine.inspect = true;
 
-    // Gets a value for config if supplied by user, or defaults to "default.conf"
-    let config = matches.value_of("config").unwrap_or("default.conf");
-    println!("Value for config: {}", config);
+    let mut app = App::new("Differential Formula")
+            .version("1.0")
+            .author("Qishen Zhang <qishen23@gmail.com>")
+            .about("Formal Specifications for Verification and Synthesis")
+            .subcommand(SubCommand::with_name("load")
+                        .about("Load a Formula file ending with .4ml")
+                        .arg(Arg::with_name("FILE")
+                            .required(true)
+                            .index(1)
+                            .help("Load a FORMULA file ending with .4ml")))
+            .subcommand(SubCommand::with_name("query")
+                        .about("Query a model")
+                        .arg(Arg::with_name("MODEL")
+                            .required(true)
+                            .index(1)
+                            .help("Query a model by matching patterns")))
+            .subcommand(SubCommand::with_name("apply")
+                        .about("Apply a model transformation given models and input terms")
+                        .arg(Arg::with_name("CMD")
+                            .required(true)
+                            .index(1)
+                            .help("Apply a model transformation given models and input terms")));
 
-    // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
-    // required we could have used an 'if let' to conditionally get the value)
-    println!("Using input file: {}", matches.value_of("INPUT").unwrap());
+    // Start the interactive CLI
+    loop {
+        print!(">>> ");
+        io::stdout().flush().unwrap();
 
-    // Vary the output based on how many times the user used the "verbose" flag
-    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
-    match matches.occurrences_of("v") {
-        0 => println!("No verbose info"),
-        1 => println!("Some verbose info"),
-        2 => println!("Tons of verbose info"),
-        3 | _ => println!("Don't be crazy"),
-    }
+        let mut s = String::new();
+        io::stdin()
+            .read_line(&mut s)
+            .ok()
+            .expect("failed to read line");
 
-    // You can handle information about subcommands by requesting their matches by name
-    // (as below), requesting just the name used, or both at the same time
-    if let Some(matches) = matches.subcommand_matches("test") {
-        if matches.is_present("debug") {
-            println!("Printing debug info...");
-        } else {
-            println!("Printing normally...");
+        // Examples: 
+        // query Path10, query LittleCycle, apply r = Add(100, LittleCycle)
+        let mut argv: Vec<_> = s.split_whitespace().collect();
+        let cmd = argv[1..argv.len()].join("");
+        if argv.contains(&"apply") {
+            argv = vec!["apply", &cmd];
+        }
+        // you have to insert the binary name since clap expects it
+        argv.insert(0, "differential-formula");
+
+        match app.get_matches_from_safe_borrow(argv) {
+            Ok(matches) => {
+                // println!("{:?}", matches);
+                execute(matches, &mut engine);
+            },
+            Err(e) => {
+                // handle error, display message, etc.
+                println!("{:?}", e);
+                continue
+            }
         }
     }
+        
+}
 
-    // more program logic goes here...
+fn execute(matches: ArgMatches, engine: &mut DDEngine) {
+    if let Some(matches) = matches.subcommand_matches("load") {
+        let file_path = matches.value_of("FILE").unwrap();
+        println!("FORUMULA file path: {:?}", file_path);
+
+        let content = fs::read_to_string(file_path).unwrap();
+        engine.install(content);
+
+    } else if let Some(matches) = matches.subcommand_matches("query") {
+        let model_name = matches.value_of("MODEL").unwrap();
+        let model = engine.env.get_model_by_name(&model_name).unwrap().clone();
+        let mut session = Session::new(model, &engine);
+        session.load();
+
+    } else if let Some(matches) = matches.subcommand_matches("apply") {
+        let transform_cmd = matches.value_of("CMD").unwrap();
+        let transformation = engine.create_model_transformation(transform_cmd);
+        let mut session = Session::new(transformation, &engine);
+        session.load();
+    }
 }
