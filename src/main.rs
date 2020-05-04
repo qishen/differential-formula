d9e4df40be9ffc252d0cd4c21827ec87092f0ef8
Qishen Zhang <qishen23@gmail.com>
2020-05-03 23:00:20 -0500

    Add more features for cmd application and update the clap dependencies.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::fs;
 use std::io;
 use std::io::Write;
 
-use clap::{Arg, ArgMatches, App, SubCommand};
+use clap::{Arg, ArgMatches, App, AppSettings, crate_version};
 
 use differential_formula::term::*;
 use differential_formula::engine::*;
@@ -16,29 +16,6 @@ fn main() {
     
     let mut engine = DDEngine::new();
     engine.inspect = true;
-
-    let mut app = App::new("Differential Formula")
-            .version("1.0")
-            .author("Qishen Zhang <qishen23@gmail.com>")
-            .about("Formal Specifications for Verification and Synthesis")
-            .subcommand(SubCommand::with_name("load")
-                        .about("Load a Formula file ending with .4ml")
-                        .arg(Arg::with_name("FILE")
-                            .required(true)
-                            .index(1)
-                            .help("Load a FORMULA file ending with .4ml")))
-            .subcommand(SubCommand::with_name("query")
-                        .about("Query a model")
-                        .arg(Arg::with_name("MODEL")
-                            .required(true)
-                            .index(1)
-                            .help("Query a model by matching patterns")))
-            .subcommand(SubCommand::with_name("apply")
-                        .about("Apply a model transformation given models and input terms")
-                        .arg(Arg::with_name("CMD")
-                            .required(true)
-                            .index(1)
-                            .help("Apply a model transformation given models and input terms")));
 
     // Start the interactive CLI
     loop {
@@ -51,36 +28,70 @@ fn main() {
             .ok()
             .expect("failed to read line");
 
-        // Examples: 
-        // query Path10, query LittleCycle, apply r = Add(100, LittleCycle)
-        let mut argv: Vec<_> = s.split_whitespace().collect();
-        let cmd = argv[1..argv.len()].join("");
-        if argv.contains(&"apply") {
-            argv = vec!["apply", &cmd];
-        }
-        // you have to insert the binary name since clap expects it
-        argv.insert(0, "differential-formula");
-
-        match app.get_matches_from_safe_borrow(argv) {
-            Ok(matches) => {
-                // println!("{:?}", matches);
-                execute(matches, &mut engine);
-            },
-            Err(e) => {
-                // handle error, display message, etc.
-                println!("{:?}", e);
-                continue
-            }
-        }
+        run_command(&s, &mut engine);
     }
         
 }
 
-fn execute(matches: ArgMatches, engine: &mut DDEngine) {
+fn run_command(cmd: &str, engine: &mut DDEngine) {
+    let mut app = App::new("Differential Formula")
+            .setting(AppSettings::WaitOnError)
+            .version(crate_version!())
+            .author("Qishen Zhang <qishen23@gmail.com>")
+            .about("Formal Specifications for Verification and Synthesis")
+            .subcommand(App::new("execute")
+                        .about("Read a file and all the commands in the file.")
+                        .arg(Arg::with_name("FILE")
+                            .required(true)
+                            .index(1)))
+            .subcommand(App::new("load")
+                        .about("Load a Formula file ending with .4ml")
+                        .arg(Arg::with_name("FILE")
+                            .required(true)
+                            .index(1)))
+            .subcommand(App::new("print")
+                        .about("Print out the content of a module in current environment.")
+                        .arg(Arg::with_name("MODULE")
+                        .required(true)
+                        .index(1)))
+            .subcommand(App::new("query")
+                        .about("Query a model")
+                        .arg(Arg::with_name("MODEL")
+                            .required(true)
+                            .index(1)))
+            .subcommand(App::new("apply")
+                        .about("Apply a model transformation given models and input terms")
+                        .arg(Arg::with_name("CMD")
+                            .required(true)
+                            .index(1)));
+
+    // Examples: 
+    // query Path10, query LittleCycle, 
+    // apply r = Add(100, LittleCycle)
+    let mut argv: Vec<_> = cmd.split_whitespace().collect();
+    let cmd = argv[1..argv.len()].join("");
+    if argv.contains(&"apply") {
+        argv = vec!["apply", &cmd];
+    }
+
+    // you have to insert the binary name since clap expects it
+    argv.insert(0, "differential-formula");
+
+    match app.try_get_matches_from_mut(argv) {
+        Ok(matches) => {
+            execute_matches(matches, engine);
+        },
+        Err(e) => {
+            // TODO: find a way to print out the err properly in terminal.
+            println!("{}", e);
+        }
+    }
+}
+
+fn execute_matches(matches: ArgMatches, engine: &mut DDEngine) {
     if let Some(matches) = matches.subcommand_matches("load") {
         let file_path = matches.value_of("FILE").unwrap();
         println!("FORUMULA file path: {:?}", file_path);
-
         let content = fs::read_to_string(file_path).unwrap();
         engine.install(content);
 
@@ -95,5 +106,27 @@ fn execute(matches: ArgMatches, engine: &mut DDEngine) {
         let transformation = engine.create_model_transformation(transform_cmd);
         let mut session = Session::new(transformation, &engine);
         session.load();
+    } else if let Some(matches) = matches.subcommand_matches("print") {
+        let module_name = matches.value_of("MODULE").unwrap();
+        if engine.env.domain_map.contains_key(module_name) {
+            let domain = engine.env.domain_map.get(module_name).unwrap();
+            println!("{:?}", domain);
+        } else if engine.env.model_map.contains_key(module_name) {
+            let model = engine.env.model_map.get(module_name).unwrap();
+            println!("{:?}", model);
+        } else if engine.env.transform_map.contains_key(module_name) {
+            let transform = engine.env.transform_map.get(module_name).unwrap();
+            println!("{:?}", transform);
+        } else {
+            println!("No module with name `{}` is found in current environment.", module_name);
+        }
+    } else if let Some(matches) = matches.subcommand_matches("execute") {
+        let file_path = matches.value_of("FILE").unwrap();
+        println!("Path of the file with commands to be executed: {:?}", file_path);
+        let content = fs::read_to_string(file_path).unwrap();
+        for line in content.split("\n") {
+            println!("---- Starting to run command: {} ----", line);
+            run_command(line, engine);
+        }
     }
 }
