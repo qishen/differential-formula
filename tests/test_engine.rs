10791a6193761bb1747f76853f820c83cc76aa70
Qishen Zhang <qishen23@gmail.com>
2020-05-06 23:54:04 -0500

    Updates on test cases according to the changes in APIs.

--- tests/test_engine.rs
+++ tests/test_engine.rs
@@ -12,103 +12,64 @@ use std::collections::HashMap;
 use rand::{Rng, SeedableRng, StdRng};
 
 
-static MODEL1: &str = "
-model m of Graph {
-    n0 is Node(0).
-    n1 is Node(1).
-    n2 is Node(2).
-    n3 is Node(3).
-    n4 is Node(4).
-
-    Edge(n0, n0).
-    Edge(n0, n1).
-    Edge(n1, n2).
-    //Edge(n2, n2).
-    Edge(n2, n3).
-    Edge(n3, n4).
-}";
-
-
-fn generate_graph_program(rules: &str, model: &str) -> String {
-    let program = format!("
-    domain Graph {{
-        Node ::= new(name: Integer).
-        Edge ::= new(src: Node, dst: Node).
-        Path ::= new(src: Node, dst: Node).
-        Line ::= new(a: Node, b: Node, c: Node, d: Node).
-        Nocycle ::= new(node: Node).
-        TwoEdge ::= new(first: Edge, second: Edge).
-
-        {}
-    }}
-
-    {}
-    ", rules, model);
+fn load_program(file_path: &str) -> DDEngine {
+    let path = Path::new(file_path);
+    let content = fs::read_to_string(path).unwrap();
     
-    program
-}
-
-/*
-fn create_session(rules: &str, model: &str) -> (Domain, Session) {
-    let program = generate_graph_program(rules, model);
-    println!("{}", program);
-
     let mut engine = DDEngine::new();
     engine.inspect = true;
-
-    // Parse string and install program in the engine.
-    let env = DDEngine::parse_string(program);
-    //  println!("{:?}", env);
-    engine.install(env);
-
-    let domain = engine.get_domain("Graph".to_string()).unwrap().clone();
-    let model = engine.get_model("m".to_string()).unwrap().clone();
-
-    let mut session = engine.create_session("Graph", Some("m"));
-    session.load_model(model);
-    (domain.clone(), session)
+    engine.install(content);
+    return engine;
 }
 
 #[test]
 fn test_ddengine_1() {
-    let rules1 = "
-        Path(a, b) :- Edge(a, b).
-        //Path(a, c) :- Path(a, b), Path(b, c).
-    ";
-
-    let (domain, mut session) = create_session(rules1, MODEL1);
+    let rule = "Path(a, b) :- Edge(a, b).
+        //Path(a, c) :- Path(a, b), Path(b, c).";
+    let mut engine = load_program("./tests/testcase/p0.4ml");
+    engine.add_rule("m", rule);
+    let m = engine.env.get_model_by_name("m").unwrap().clone();
+    let mut session = Session::new(m, &engine);
+    session.load();
 }
 
 #[test]
 fn test_ddengine_2() {
-    let rules2 = "
-        Edge(a, c) :- Edge(a, b), Edge(b, c).
-    ";
-    
-    let (domain, mut session) = create_session(rules2, MODEL1);
-    
-    let edge45 = session.parse_term_str("Edge(Node(4), Node(5))").unwrap();
-    let edge56 = session.parse_term_str("Edge(Node(5), Node(6))").unwrap();
-
-    session.add_terms(vec![edge45, edge56]);
+    let rule = "Edge(a, c) :- Edge(a, b), Edge(b, c).";
+    let mut engine = load_program("./tests/testcase/p0.4ml");
+    engine.add_rule("m", rule);
+    let m = engine.env.get_model_by_name("m").unwrap().clone();
+    let mut session = Session::new(m, &engine);
+    session.load();
 }
 
 #[test]
 fn test_ddengine_3() {
-    let rules3 = "
+    let rule = "
         Edge(a, d) :- Edge(a, b), Edge(b, c), Edge(c, d).
     ";
-
-    let (domain, mut session) = create_session(rules3, MODEL1);
+    let mut engine = load_program("./tests/testcase/p0.4ml");
+    engine.add_rule("m", rule);
+    let m = engine.env.get_model_by_name("m").unwrap().clone();
+    let mut session = Session::new(m, &engine);
+    session.load();
 }
 
 #[test]
 fn test_ddengine_4() {
-    let rules4 = "
+    let rule = "
         Path(a, b) :- Edge(a, b).
         Path(a, c) :- Path(a, b), Path(b, c).
         Nocycle(u) :- u is Node(_), no Path(u, u).
     ";
+
+    let mut engine = load_program("./tests/testcase/p0.4ml");
+    engine.add_rule("m", rule);
+    let m = engine.env.get_model_by_name("m").unwrap().clone();
+    let mut session = Session::new(m, &engine);
+    session.load();
+
+    /*
 
     let (domain, mut session) = create_session(rules4, MODEL1);
 
@@ -122,58 +83,83 @@ fn test_ddengine_4() {
     session.add_terms(vec![node5, edge45]);
     session.add_term(edge22);
     session.remove_term(edge00);
+    */
 }
 
 #[test]
 fn test_ddengine_5() {
-    let rules5 = "
+    let rule = "
         Line(a, b, c, d) :- Edge(a, b), Edge(c, d).
     ";
 
-    let (domain, mut session) = create_session(rules5, MODEL1);
+    let mut engine = load_program("./tests/testcase/p0.4ml");
+    engine.add_rule("m", rule);
+    let m = engine.env.get_model_by_name("m").unwrap().clone();
+    let mut session = Session::new(m, &engine);
+    session.load();
 }
 
 
 #[test]
 fn test_ddengine_6() {
-    let rules6 = "
+    let rule = "
         TwoEdge(x, y) :- x is Edge(a, b), y is Edge(b, c).
     ";
-
-    let (domain, mut session) = create_session(rules6, MODEL1);
+    let mut engine = load_program("./tests/testcase/p0.4ml");
+    engine.add_rule("m", rule);
+    let m = engine.env.get_model_by_name("m").unwrap().clone();
+    let mut session = Session::new(m, &engine);
+    session.load();
 }
 
 #[test]
 fn test_ddengine_6x() {
-    let rules6x = "
+    let rule = "
         TwoEdge(x, y) :- x is Edge(_, _), y is Edge(_, _).
     ";
 
-    let (domain, mut session) = create_session(rules6x, MODEL1);
+    let mut engine = load_program("./tests/testcase/p0.4ml");
+    engine.add_rule("m", rule);
+    let m = engine.env.get_model_by_name("m").unwrap().clone();
+    let mut session = Session::new(m, &engine);
+    session.load();
 }
 
 #[test]
 fn test_ddengine_7() {
-    let rules7 = "
+    let rule = "
         TwoEdge(x, x, square) :- x is Edge(c, d), 
                                  aggr = count({Edge(a, a), b | Edge(a, b)}), 
                                  square = aggr * aggr, aggr * 2 = 20 .
     ";
 
+    let mut engine = load_program("./tests/testcase/p0.4ml");
+    engine.add_rule("m", rule);
+    let m = engine.env.get_model_by_name("m").unwrap().clone();
+    let mut session = Session::new(m, &engine);
+    session.load();
+
+    /*
     let (domain, mut session) = create_session(rules7, MODEL1);
 
     let edge45 = session.parse_term_str("Edge(Node(4), Node(5))").unwrap();
     let edge56 = session.parse_term_str("Edge(Node(5), Node(6))").unwrap();
 
-    session.add_terms(vec![edge45, edge56]);
+    session.add_terms(vec![edge45, edge56]);*/
 }
 
 #[test]
 fn test_ddengine_7x() {
-    let rules7x = "
+    let rule = "
         Node(num) :- x is Node(_), num = aggr + 100, aggr = maxAll(1000, { k | Edge(_, Node(k)) }).
     ";
+    let mut engine = load_program("./tests/testcase/p0.4ml");
+    engine.add_rule("m", rule);
+    let m = engine.env.get_model_by_name("m").unwrap().clone();
+    let mut session = Session::new(m, &engine);
+    session.load();
 
+    /*
     let (domain, mut session) = create_session(rules7x, MODEL1);
 
     let edge34 = session.parse_term_str("Edge(Node(3), Node(4))").unwrap();
@@ -181,57 +167,60 @@ fn test_ddengine_7x() {
 
     session.add_term(edge56.clone());
 
-    session.remove_terms(vec![edge34, edge56]);
+    session.remove_terms(vec![edge34, edge56]);*/
 }
 
 #[test]
 fn test_ddengine_8() {
-    let rules8 = "
+    let rule = "
         Edge(x.src, d) :- x is Edge(a, b), y is Edge(b, c), Edge(y.dst, d).
     ";
 
-    let (domain, mut session) = create_session(rules8, MODEL1);
+    let mut engine = load_program("./tests/testcase/p0.4ml");
+    engine.add_rule("m", rule);
+    let m = engine.env.get_model_by_name("m").unwrap().clone();
+    let mut session = Session::new(m, &engine);
+    session.load();
 }
 
 #[test]
 fn test_ddengine_8x() {
-    let rules8x = "
+    let rule = "
         Edge(a, c) :- x is Edge(a, y.src), y is Edge(x.dst, c).
     ";
-
-    let (domain, mut session) = create_session(rules8x, MODEL1);
+    let mut engine = load_program("./tests/testcase/p0.4ml");
+    engine.add_rule("m", rule);
+    let m = engine.env.get_model_by_name("m").unwrap().clone();
+    let mut session = Session::new(m, &engine);
+    session.load();
 }
 
 #[test]
 // TODO: Fix the constraint classification in rule.
 fn test_ddengine_9() {
     // Let's try a nested aggregation.
-    let rules9 = "
+    let rule = "
     TwoEdge(x, x, num) :- x is Edge(c, d), aggr1 = count({ n | n is Node(_), aggr2 = count({ x | x is Edge(a, b) }) }), num = aggr1 * 100 .
     ";
-
-    let (domain, mut session) = create_session(rules9, MODEL1);
+    let mut engine = load_program("./tests/testcase/p0.4ml");
+    engine.add_rule("m", rule);
+    let m = engine.env.get_model_by_name("m").unwrap().clone();
+    let mut session = Session::new(m, &engine);
+    session.load();
 }
 
 
 #[test]
 fn test_ddengine_10() {
-    let rules10 = "
+    let rule = "
     Node(num) :- num = aggr * 1000, aggr = count({Edge(a, a), b | Edge(a, b)}).
     ";
 
-    let (domain, mut session) = create_session(rules10, MODEL1);
-}
-*/
-
-fn load_program(file_path: &str) -> DDEngine {
-    let path = Path::new(file_path);
-    let content = fs::read_to_string(path).unwrap();
-    
-    let mut engine = DDEngine::new();
-    engine.inspect = true;
-    engine.install(content);
-    return engine;
+    let mut engine = load_program("./tests/testcase/p0.4ml");
+    engine.add_rule("m", rule);
+    let m = engine.env.get_model_by_name("m").unwrap().clone();
+    let mut session = Session::new(m, &engine);
+    session.load();
 }
 
 #[test]
@@ -314,10 +303,10 @@ fn test_social_network() {
 #[test]
 fn test_incremental_transitive_closure() {
     let mut engine = load_program("./tests/testcase/p0.4ml");
-    let seed: &[_] = &[1, 2, 3, 4];
+    let seed: &[_] = &[1, 2, 3, 0];
     let mut rng1: StdRng = SeedableRng::from_seed(seed);
 
-    // Example: cargo test test_incremental_transitive_closure -- --nocapture 100 100
+    // Example: cargo test test_incremental_transitive_closure -- --nocapture 100 100 1
     let nodes: usize = std::env::args().nth(3).unwrap_or("100".to_string()).parse().unwrap();
     let edges: usize = std::env::args().nth(4).unwrap_or("50".to_string()).parse().unwrap();
     let updated_edges: usize = std::env::args().nth(5).unwrap_or("20".to_string()).parse().unwrap();
@@ -340,7 +329,7 @@ fn test_incremental_transitive_closure() {
     let d1 = timer.elapsed();
 
     let mut updated_terms = vec![];
-    for i in 0 .. updated_edges {
+    for _ in 0 .. updated_edges {
         let num1 = rng1.gen_range(0, nodes);
         let num2 = rng1.gen_range(0, nodes);
         let edge_str = format!("Edge(Node({}), Node({}))", num1, num2);
@@ -348,10 +337,15 @@ fn test_incremental_transitive_closure() {
         updated_terms.push(edge_arc);
     }
     let timer = std::time::Instant::now();
-    session.add_terms(updated_terms);
+    session.add_terms(updated_terms.clone());
     let d2 = timer.elapsed();
+
+    let timer = std::time::Instant::now();
+    session.remove_terms(updated_terms);
+    let d3 = timer.elapsed();
 
     // Print out results for benchmark.
     println!("Initial computation finished in {:?}", d1);
     println!("Updates finished in {:?}", d2);
+    println!("Remove all updates in {:?}", d3);
 }
