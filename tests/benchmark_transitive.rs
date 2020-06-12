91ee5996b3ab65c5b16e74e119d1a36fd45c532e
Qishen Zhang <qishen23@gmail.com>
2020-06-11 23:58:12 -0500

    Use closure as parameter to test different data structure in graph computation.

--- tests/benchmark_transitive.rs
+++ tests/benchmark_transitive.rs
@@ -1,15 +1,17 @@
+#![feature(core_intrinsics)]
 #![type_length_limit="1120927"]
 extern crate rand;
 
+use std::intrinsics::type_name;
 use std::marker::*;
-use std::sync::Arc;
+use std::sync::*;
 use std::hash::Hash;
 use std::collections::BTreeMap;
 use rand::{Rng, SeedableRng, StdRng};
 use num::*;
 
 use differential_dataflow::*;
-use differential_dataflow::input::Input;
+use differential_dataflow::input::*;
 use differential_dataflow::operators::Iterate;
 use differential_dataflow::operators::Join;
 use differential_dataflow::operators::Threshold;
@@ -20,35 +22,13 @@ use differential_dataflow::hashable::*;
 
 use differential_formula::term::*;
 use differential_formula::engine::*;
-use differential_formula::util::*;
+use differential_formula::util::map::*;
+use differential_formula::parser::combinator::*;
 
 use timely::dataflow::*;
 use timely::dataflow::scopes::*;
 use timely::worker::*;
 use timely::communication::*;
-
-#[test]
-fn main() {
-    println!("{:?}", std::env::args());
-
-    //transitive();
-    //transitive_formula();
-
-    // RUST_TEST_NOCAPTURE=1 cargo test main -- 500 3000 2 false
-    hops();
-    hops_formula();
-    hops_formula_hashmap();
-    hops_differential_formula();
-
-    //let convert_to_integer = |x| x;
-    //let tran_closure_dataflow = |edges| transitive_closure_dataflow(edges);
-
-    /*graph_computation(
-        |x| x,
-        |edges| transitive_closure_dataflow(&edges)
-    );*/
-}
-
 
 fn hops_dataflow<G, N>(edges: &Collection<G, (N, N)>, hops: i32) -> Collection<G, (N, N)>
 where
@@ -74,7 +54,7 @@ fn transitive_closure_dataflow<G, N>(edges: &Collection<G, (N, N)>) -> Collectio
 where
     G: Scope,
     G::Timestamp: Lattice+Ord,
-    N: ExchangeData+Hash,
+    N: ExchangeData+Hashable,
     //R: ExchangeData+Abelian,
     //R: Mul<R, Output=R>,
     //R: From<i8>,
@@ -86,64 +66,148 @@ where
             .join(&inner)
             .map(|(_y, (x, z))| (x, z))
             .concat(&edges)
-            .distinct()
+            //.distinct()
     })
 }
 
+#[test]
+fn main() {
+    println!("{:?}", std::env::args());
 
-fn graph_computation<G, N, F1, F2>(convert: F1, dataflow: F2) 
-where
-    F1: Fn(usize) -> N + Send + Sync + 'static,
-    F2: Fn(&Collection<Child<Worker<Allocator>, i32>, (N, N)>) -> Collection<G, (N, N)> + Send + Sync + 'static,
-    G: Scope<Timestamp=i32>,
-    G::Timestamp: Lattice+Ord,
-    N: ExchangeData+Hash,
-{
-    timely::execute_from_args(std::env::args(), move |worker| {
-        let timer = ::std::time::Instant::now();
+    //transitive();
+    //transitive_formula();
 
-        let index = worker.index();
-        let peers = worker.peers();
+    // RUST_TEST_NOCAPTURE=1 cargo test main -- 500 3000 2 false
+    // hops();
+    // hops_formula();
+    // hops_formula_hashmap();
+    // hops_differential_formula();
 
-        // Set default numbers for nodes and edges if not specified in cmd arguments.
-        let nodes: usize = std::env::args().nth(1).unwrap_or("100".to_string()).parse().unwrap_or(100);
-        let edges: usize = std::env::args().nth(2).unwrap_or("20".to_string()).parse().unwrap_or(20);
+    // graph_computation(convert_to_integer, tran_closure_dataflow);
 
-        let (mut input, probe) = worker.dataflow(|scope| {
-            let (input, edges) = scope.new_collection();
-            let probe = dataflow(&edges).probe();
-            (input, probe)
-        });
+    // graph_computation(
+    //     convert_to_integer,
+    //     Arc::new(|edges: Collection<Child<Worker<Allocator>, i32>, (usize, usize)>| transitive_closure_dataflow(&edges))
+    // );
 
-        let seed: &[_] = &[1, 2, 3, index];
-        let mut rng1: StdRng = SeedableRng::from_seed(seed);    // rng for edge additions
+    let program = "
+        domain Graph {
+            Node ::= new(name: Integer).
+            Edge ::= new(src: Node, dst: Node).
+            Path ::= new(src: Node, dst: Node).
+            Line ::= new(a: Node, b: Node, c: Node, d: Node).
+            Nocycle ::= new(node: Node).
+            TwoEdge ::= new(first: Edge, second: Edge).
 
-        // Load up graph data. Round-robin among workers.
-        for _ in 0 .. (edges / peers) + if index < (edges % peers) { 1 } else { 0 } {
-            let num1 = rng1.gen_range(0, nodes);
-            let num2 = rng1.gen_range(0, nodes);
-            let node1 = convert(num1);
-            let node2 = convert(num2);
-            input.update((node1, node2), 1)
+            Edge(x, z) :- Edge(x, y), Edge(y, z).
         }
+        ".to_string();
 
-        input.advance_to(1);
-        input.flush();
-        worker.step_while(|| probe.less_than(input.time()));
+    let mut engine = DDEngine::new();
 
-        if index == 0 {
-            println!("Graph computation finished after {:?}", timer.elapsed());
-        }
+    // Parse string and install program in the engine.
+    engine.install(program);
+    let node_type = engine.env.get_domain_by_name("Graph").unwrap().get_type(&"Node".to_string());
+    let node_type2 = node_type.clone();
+    let env = engine.env.clone();
 
-    }).unwrap();
+    // Some lambda functions to convert `usize` to other types.
+    let convert_to_int = |x: usize| { x };
+
+    let convert_to_wrapped_int = |num: usize| {
+        let v1: UnsignedWrapper<usize> = num.into();
+        let v2: HashableWrapper<UnsignedWrapper<usize>> = v1.into();
+        return OrdWrapper { item: v2 };
+    };
+
+    let convert_to_bigint = |x: usize| { BigInt::from(x) };
+
+    let convert_to_wrapped_bigint = |num: usize| {
+        let v = BigInt::from(num);
+        let v1: HashableWrapper<BigInt> = v.into();
+        return OrdWrapper { item: v1 };
+    };
+
+    let convert_to_term = move |x: usize| {
+        let graph_domain = env.get_domain_by_name("Graph").unwrap();
+        let term_ast = term(&format!("Node({}){}", x, "~")[..]).unwrap().1;
+        let term = term_ast.to_term(graph_domain);
+        let node_with_hash: HashableWrapper<Term> = term.clone().into();
+        let wrapped_node = OrdWrapper { item: node_with_hash };
+        return wrapped_node;
+    };
+
+    let convert_to_int_hashmap = |num: usize| {
+        let mut map = BTreeMap::new();
+        map.insert(num, num);
+        //map.insert(num+1, num+1);
+        return map;
+    };
+
+    let convert_to_int_quick_hashmap = |num: usize| {
+        let mut map = BTreeMap::new();
+        map.insert(num, num);
+        //map.insert(num+1, num+1);
+        let wrapped_map: QuickHashOrdMap<usize, usize> = map.into();
+        return wrapped_map;
+    };
+
+    let convert_to_term_hashmap = move |num: usize| {
+        let _x: Term = Variable::new("x".to_string(), vec![]).into();
+        let _y: Term = Variable::new("y".to_string(), vec![]).into();
+        let x = Arc::new(_x);
+        let y = Arc::new(_y);
+
+        let atom: Term = AtomEnum::Int(BigInt::from(num)).into();
+        let node: Term = Composite::new(node_type.clone(), vec![Arc::new(atom)], None).into();
+
+        let mut map = BTreeMap::new();
+        map.insert(x.clone(), Arc::new(node.clone()));
+        //map.insert(y.clone(), Arc::new(node.clone()));
+        return map;
+    };
+
+    let convert_to_term_quick_hashmap = move |num: usize| {
+        let _x: Term = Variable::new("x".to_string(), vec![]).into();
+        let _y: Term = Variable::new("y".to_string(), vec![]).into();
+        let x = Arc::new(_x);
+        let y = Arc::new(_y);
+
+        let atom: Term = AtomEnum::Int(BigInt::from(num)).into();
+        let node: Term = Composite::new(node_type2.clone(), vec![Arc::new(atom)], None).into();
+
+        let mut map = BTreeMap::new();
+        map.insert(x.clone(), Arc::new(node.clone()));
+        //map.insert(y.clone(), Arc::new(node.clone()));
+
+        let wrapped_map: QuickHashOrdMap<Arc<Term>, Arc<Term>> = map.into();
+        return wrapped_map;
+    };
+
+    hops_computation(convert_to_int);
+    hops_computation(convert_to_wrapped_int);
+    hops_computation(convert_to_bigint);
+    hops_computation(convert_to_wrapped_bigint);
+    hops_computation(convert_to_term);
+    hops_computation(convert_to_int_hashmap);
+    hops_computation(convert_to_int_quick_hashmap);
+    hops_computation(convert_to_term_hashmap);
+    hops_computation(convert_to_term_quick_hashmap);
+    
+    hops_differential_formula();
 }
 
-fn hops() {
-    timely::execute_from_args(std::env::args(), move |worker| {
 
+fn hops_computation<F, N>(convert: F)
+where 
+    F: Fn(usize) -> N + Sync + Send + 'static,
+    N: ExchangeData+Hashable,
+{
+    timely::execute_from_args(std::env::args(), move |worker| {
         let index = worker.index();
         let peers = worker.peers();
 
+        // Default configuration with 2 hops and no inspection.
         let nodes: usize = std::env::args().nth(2).unwrap_or("100".to_string()).parse().unwrap_or(100);
         let edges: usize = std::env::args().nth(3).unwrap_or("20".to_string()).parse().unwrap_or(20);
         let hops: i32 = std::env::args().nth(4).unwrap_or("2".to_string()).parse().unwrap_or(2);
@@ -161,14 +225,14 @@ fn hops() {
 
         let seed: &[_] = &[1, 2, 3, index];
         let mut rng1: StdRng = SeedableRng::from_seed(seed);    // rng for edge additions
-        //let mut rng2: StdRng = SeedableRng::from_seed(seed);    // rng for edge deletions
 
         // Load up graph data. Round-robin among workers.
         for _ in 0 .. (edges / peers) + if index < (edges % peers) { 1 } else { 0 } {
             // Using BigInt is slower than primitive integer.
-            let num1 = BigInt::from(rng1.gen_range(0, nodes));
-            let num2 = BigInt::from(rng1.gen_range(0, nodes));
-            input.update((num1.clone(), num2.clone()), 1);
+            let num1 = rng1.gen_range(0, nodes);
+            let num2 = rng1.gen_range(0, nodes);
+            let edge = (convert(num1), convert(num2));
+            input.update(edge, 1);
             if inspect {
                 println!("Initial tuple {:?}", (num1, num2));
             }
@@ -181,200 +245,11 @@ fn hops() {
         worker.step_while(|| probe.less_than(input.time()));
 
         if index == 0 {
-            println!("Compute hops in integers finished after {:?}", timer.elapsed());
+            println!("Compute hops in {} finished after {:?}", type_name::<N>(), timer.elapsed());
         }
 
     }).unwrap();
 
-}
-
-// Use a tuple of two Formula terms as data structure but still run on native differential dataflow.
-fn hops_formula() {
-    timely::execute_from_args(std::env::args(), move |worker| {
-
-        let index = worker.index();
-        let peers = worker.peers();
-
-        let nodes: usize = std::env::args().nth(2).unwrap_or("100".to_string()).parse().unwrap_or(100);
-        let edges: usize = std::env::args().nth(3).unwrap_or("200".to_string()).parse().unwrap_or(200);
-        let hops: i32 = std::env::args().nth(4).unwrap_or("2".to_string()).parse().unwrap_or(2);
-        let inspect: bool = std::env::args().nth(5).unwrap_or("false".to_string()).parse().unwrap_or(false);
-
-        let (mut input, probe) = worker.dataflow(|scope| {
-            let (input, edges) = scope.new_collection();
-            let output = hops_dataflow(&edges, hops);
-            if inspect {
-                output.inspect(|x| println!("The output: {:?}", x));
-            }
-            let probe = output.probe();
-            (input, probe)
-        });
-
-        let program = "
-            domain Graph {
-                Node ::= new(name: Integer).
-                Edge ::= new(src: Node, dst: Node).
-                Path ::= new(src: Node, dst: Node).
-                Line ::= new(a: Node, b: Node, c: Node, d: Node).
-                Nocycle ::= new(node: Node).
-                TwoEdge ::= new(first: Edge, second: Edge).
-
-                Edge(x, z) :- Edge(x, y), Edge(y, z).
-            }
-            ".to_string();
-
-        let mut engine = DDEngine::new();
-
-        // Parse string and install program in the engine.
-        engine.install(program);
-        let m = engine.create_empty_model("m", "Graph");
-        let session = Session::new(m, &engine);
-
-        let seed: &[_] = &[1, 2, 3, index];
-        let mut rng1: StdRng = SeedableRng::from_seed(seed);    // rng for edge additions
-
-        // Load up graph data. Round-robin among workers.
-        for _ in 0 .. (edges / peers) + if index < (edges % peers) { 1 } else { 0 } {
-            let num1 = rng1.gen_range(0, nodes);
-            let num2 = rng1.gen_range(0, nodes);
-
-            let node1 = session.create_term(&format!("Node({})", num1)).unwrap();
-            let node2 = session.create_term(&format!("Node({})", num2)).unwrap();
-
-            let node1_with_hash: HashableWrapper<Term> = node1.clone().into();
-            let node2_with_hash: HashableWrapper<Term> = node2.clone().into();
-            // println!("{:?}", node1_with_hash);
-            let wrapped_node1 = OrdWrapper { item: node1_with_hash };
-            let wrapped_node2 = OrdWrapper { item: node2_with_hash };
-
-            // let edge_tuple = (node1, node2);
-            let edge_tuple = (wrapped_node1, wrapped_node2);
-
-            if inspect {
-                println!("Initial term tuple {:?}", edge_tuple);
-            }
-
-            input.insert(edge_tuple);
-        }
-
-        let timer = ::std::time::Instant::now();
-
-        input.advance_to(1);
-        input.flush();
-        worker.step_while(|| probe.less_than(input.time()));
-
-        if index == 0 {
-            println!("Compute hops in terms finished after {:?}", timer.elapsed());
-        }
-
-    }).unwrap();
-}
-
-// Use native differential formula but each node is a hashmap with formula terms.
-fn hops_formula_hashmap() {
-    timely::execute_from_args(std::env::args(), move |worker| {
-
-        let index = worker.index();
-        let peers = worker.peers();
-
-        let nodes: usize = std::env::args().nth(2).unwrap_or("100".to_string()).parse().unwrap_or(100);
-        let edges: usize = std::env::args().nth(3).unwrap_or("200".to_string()).parse().unwrap_or(200);
-        let hops: i32 = std::env::args().nth(4).unwrap_or("2".to_string()).parse().unwrap_or(2);
-        let inspect: bool = std::env::args().nth(5).unwrap_or("false".to_string()).parse().unwrap_or(false);
-
-        let (mut input, probe) = worker.dataflow(|scope| {
-            let (input, edges) = scope.new_collection();
-            let output = hops_dataflow(&edges, hops);
-            if inspect {
-                output.inspect(|x| println!("The output: {:?}", x));
-            }
-            let probe = output.probe();
-            (input, probe)
-        });
-
-        let program = "
-            domain Graph {
-                Node ::= new(name: Integer).
-                Edge ::= new(src: Node, dst: Node).
-                Path ::= new(src: Node, dst: Node).
-                Line ::= new(a: Node, b: Node, c: Node, d: Node).
-                Nocycle ::= new(node: Node).
-                TwoEdge ::= new(first: Edge, second: Edge).
-
-                Edge(x, z) :- Edge(x, y), Edge(y, z).
-            }
-            ".to_string();
-
-        let mut engine = DDEngine::new();
-
-        if inspect {
-            engine.inspect = true;
-        } else {
-            engine.inspect = false;
-        }
-
-        engine.install(program);
-
-        let domain = engine.env.get_domain_by_name("Graph").unwrap().clone();
-        let node = domain.get_type(&"Node".to_string());
-
-        let seed: &[_] = &[1, 2, 3, index];
-        let mut rng1: StdRng = SeedableRng::from_seed(seed);    // rng for edge additions
-
-        let _x: Term = Variable::new("x".to_string(), vec![]).into();
-        let _y: Term = Variable::new("y".to_string(), vec![]).into();
-        let x = Arc::new(_x);
-        let y = Arc::new(_y);
-
-        // Load up graph data. Round-robin among workers.
-        for _ in 0 .. (edges / peers) + if index < (edges % peers) { 1 } else { 0 } {
-            let num1 = rng1.gen_range(0, nodes);
-            let num2 = rng1.gen_range(0, nodes);
-            let atom1: Term = AtomEnum::Int(BigInt::from(num1)).into();
-            let atom2: Term = AtomEnum::Int(BigInt::from(num2)).into();
-            let node1: Term = Composite::new(node.clone(), vec![Arc::new(atom1)], None).into();
-            let node2: Term = Composite::new(node.clone(), vec![Arc::new(atom2)], None).into();
-
-            // Each map has an unique form that is used to compare ordering when two maps have the same
-            // hash then just check if they have same unique form.
-            let mut map1 = BTreeMap::new();
-            map1.insert(x.clone(), Arc::new(node1.clone()));
-            map1.insert(y.clone(), Arc::new(node1.clone()));
-            //println!("Hash of {:?} is {}", map1, map1.hashed());
-            let wrapped_map1: QuickHashOrdMap<Arc<Term>, Arc<Term>> = map1.into();
-
-            let mut map2 = BTreeMap::new();
-            map2.insert(x.clone(), Arc::new(node2.clone()));
-            map2.insert(y.clone(), Arc::new(node2.clone()));
-            //println!("Hash of {:?} is {}", map2, map2.hashed());
-            let wrapped_map2: QuickHashOrdMap<Arc<Term>, Arc<Term>> = map2.into();
-
-            // let map1_with_hash: HashableWrapper<OrdMap<Term, Term>> = map1.clone().into();
-            // let map2_with_hash: HashableWrapper<OrdMap<Term, Term>> = map2.clone().into();
-            // println!("{:?}", node1_with_hash);
-            // let wrapped_map1 = OrdWrapper { item: map1_with_hash };
-            // let wrapped_map2 = OrdWrapper { item: map2_with_hash };
-
-            let edge_tuple = (wrapped_map1, wrapped_map2);
-            // let edge_tuple = (wrapped_map1, wrapped_map2);
-
-            if inspect {
-                println!("Initial term tuple {:?}", edge_tuple);
-            }
-
-            input.insert(edge_tuple);
-        }
-
-        let timer = ::std::time::Instant::now();
-
-        input.advance_to(1);
-        input.flush();
-        worker.step_while(|| probe.less_than(input.time()));
-
-        if index == 0 {
-            println!("Compute hops in hashmaps finished after {:?}", timer.elapsed());
-        }
-    }).unwrap();
 }
 
 fn hops_differential_formula() {
@@ -433,7 +308,11 @@ fn hops_differential_formula() {
     println!("differential-formula finished after {:?}", timer.elapsed());
 }
 
-fn transitive() {
+fn transitive_computation<F, N>(convert: F) 
+where 
+    F: Fn(usize) -> N + Sync + Send + 'static,
+    N: ExchangeData+Hashable,
+{
     timely::execute_from_args(std::env::args(), move |worker| {
         let timer = ::std::time::Instant::now();
 
@@ -450,14 +329,14 @@ fn transitive() {
         });
 
         let seed: &[_] = &[1, 2, 3, index];
-        let mut rng1: StdRng = SeedableRng::from_seed(seed);    // rng for edge additions
-        //let mut rng2: StdRng = SeedableRng::from_seed(seed);    // rng for edge deletions
+        let mut rng1: StdRng = SeedableRng::from_seed(seed); // rng for edge additions
 
         // Load up graph data. Round-robin among workers.
         for _ in 0 .. (edges / peers) + if index < (edges % peers) { 1 } else { 0 } {
             let num1 = rng1.gen_range(0, nodes);
             let num2 = rng1.gen_range(0, nodes);
-            input.update((num1, num2), 1)
+            let edge = (convert(num1), convert(num2));
+            input.update(edge, 1);
         }
 
         input.advance_to(1);
@@ -465,7 +344,7 @@ fn transitive() {
         worker.step_while(|| probe.less_than(input.time()));
 
         if index == 0 {
-            println!("Compute transitive closure in integers finished after {:?}", timer.elapsed());
+            println!("Compute transitive closure in {} finished after {:?}", type_name::<N>(), timer.elapsed());
         }
 
     }).unwrap();
