4cc6c09ae809d3af753321e25e9495268deb1c85
Qishen Zhang <qishen23@gmail.com>
2021-04-26 18:49:25 -0500

    Update some examples.

--- symbolic/sorting.py
+++ symbolic/sorting.py
@@ -1,6 +1,15 @@
 from z3 import *
 
 """
+Problem definition:
+Try to find a list of four ints that cannot be sorted by swapping two adjacent items as 
+counter example
+
+The problem is modeled in FORMULA with recursion, negation and infinite integer domain.
+A naive solution is to run concrete execution in every input, which is not possible because
+the integer domain is infinite. We run symbolic execution with symbolic values and dump the
+intermediate results into z3 solver to prove the absence of counter example.
+
 domain Sorting {
 
     input ::= new (Integer, Integer, Integer, Integer).
@@ -14,6 +23,13 @@ domain Sorting {
     trace(W, Y, X, Z) :- trace(W, X, Y, Z), X > Y.
     trace(W, X, Z, Y) :- trace(W, X, Y, Z), Y > Z.
 }
+
+// Define a partial model with only one fact. We can define more facts but one is more than enough
+// to generate counter example if it exists.
+partial model of Sorting {
+    input(s1, s2, s3, s4).
+}
+
 """
 
 def sorting_symbexec_single_step():
@@ -32,7 +48,6 @@ def sorting_symbexec_single_step():
     """
 
     s1, s2, s3, s4 = Ints('s1 s2 s3 s4')
-
     s = Solver()
     # Execute rules only once with 3 possibilities.
     s.add(
@@ -150,4 +165,108 @@ def sorting_symbexec():
         print(model)
 
 
+
+
+# Check if a counter example cntrexmp(W, X, Y, Z) exists or the absence of trace(A, B, C, D) 
+# where A <= B, B <= C, C <= D.
+# cntrexmp(W, X, Y, Z) :- input(W, X, Y, Z), no { A, B, C, D | trace(A, B, C, D), A <= B, B <= C, C <= D }.
+# trace(W, X, Y, Z) :- input(W, X, Y, Z).
+# trace(X, W, Y, Z) :- trace(W, X, Y, Z), W > X.
+# trace(W, Y, X, Z) :- trace(W, X, Y, Z), X > Y.
+# trace(W, X, Z, Y) :- trace(W, X, Y, Z), Y > Z.
+
+# input ::= new (int, int, int, int)
+Input = Datatype("Input")
+Input.declare('input', ('one', IntSort()), ('two', IntSort()), ('three', IntSort()), ('four', IntSort()))
+Input = Input.create()
+# trace ::= new (int, int, int, int)
+Trace = Datatype("Trace")
+Trace.declare('trace', ('one', IntSort()), ('two', IntSort()), ('three', IntSort()), ('four', IntSort()))
+Trace = Trace.create()
+
+class SortingFPDatatypeState:
+    variables = []
+    trace = None
+    constraints = set()
+
+    def __init__(self, variables, constraints):
+        self.constraints = constraints
+        self.variables = variables
+        self.trace = Trace.trace(self.variables)
+
+    def __eq__(self, other):
+        return self.trace == other.trace and self.constraints == other.constraints
+
+    def __hash__(self):
+        return hash((self.trace, tuple(self.constraints)))
+
+    def __str__(self):
+        contraint_inner = ", ".join([str(i) for i in self.constraints])
+        return "{}, [{}]".format(self.trace, contraint_inner)
+
+    def execute(self):
+        states = []
+        for i in range(0, len(self.variables)-1):
+            variables = list(self.variables)
+            # Abandon the state if (a < b) exists in constraints while we want to add (a > b).
+            # TODO: Check other conflicts in the constraint set.
+            if (variables[i+1] > variables[i]) not in self.constraints:
+                new_constraint = variables[i] > variables[i+1]
+                tmp = variables[i]
+                variables[i] = variables[i+1]
+                variables[i+1] = tmp
+                constraints = set(self.constraints)
+                constraints.update([new_constraint])
+                new_state = SortingFPDatatypeState(variables, constraints)
+                states.append(new_state)
+        return states
+
+def sorting_symbexec_datatype():
+    s1, s2, s3, s4 = Ints('s1 s2 s3 s4')
+    initial_state = SortingFPDatatypeState([s1, s2, s3, s4], [])
+
+    state_set = set()
+    stack = []
+    state_set.add(initial_state)
+    stack.append(initial_state)
+
+    while len(stack) != 0:
+        state = stack.pop()
+        for new_state in state.execute():
+            if new_state not in state_set:
+                stack.append(new_state)
+                state_set.add(new_state)
+
+    # Find a input that eventually outputs a sorted trace after symbolic execution.
+    conjunctions = [] 
+    for state in state_set:
+        variables = state.variables
+        # where A <= B, B <= C, C <= D in trace(A, B, C, D).
+        goal_constraints = []
+        for i in range(0, len(variables)-1):
+            goal_constraints.append(variables[i] < variables[i+1])
+        conjunctions.append(And(list(state.constraints) + goal_constraints))
+        print(state)
+
+    # TODO: How to access members inside trace?
+    # print(initial_trace['one'])
+
+    s = Solver()
+    # Check if there exists an assignment that fails to reach the goal.
+    # The goal is to find four distinct integers that all derived symbolic states 
+    # from it do not stand for the goal 
+    s.add(Exists([s1, s2, s3, s4], 
+        And(Distinct(s1, s2, s3, s4), Not(Or(conjunctions)))
+    ))
+
+    # Find an assignment for s1, s2, s3, s4 that the traces from symbolic execution reach the goal.
+    # s.add(Or(conjunctions))
+
+    print("Constraint generation finished and start model generation...")
+    print(s.check())
+    if s.check() == sat:
+        model = s.model()
+        print(model)
+
 sorting_symbexec()
+sorting_symbexec_datatype()
--- symbolic/toy_examples.py
+++ symbolic/toy_examples.py
@@ -11,30 +11,29 @@ def test_simplify():
 
 
 def test_treelist():
-
+    # Define datatypes that are mutually recursive
     Tree = Datatype('Tree')
     TreeList = Datatype('TreeList')
 
+    # Datatype Tree has two constructors.
     Tree.declare('leaf', ('val', IntSort()))
     Tree.declare('node', ('left', TreeList), ('right', TreeList))
 
+    # Datatype TreeList has two constructors. 
     TreeList.declare('nil')
     TreeList.declare('cons', ('car', Tree), ('cdr', TreeList))
 
     Tree, TreeList = CreateDatatypes(Tree, TreeList)
 
-    # t1  = Tree.leaf(10)
-    # tl1 = TreeList.cons(t1, TreeList.nil)
-    # t2  = Tree.node(tl1, TreeList.nil)
-    # print(t2)
-    # print(Tree.val(t1))
-    # print(simplify(Tree.val(t1)))
+    t1  = Tree.leaf(10)
+    tl1 = TreeList.cons(t1, TreeList.nil)
+    t2  = Tree.node(tl1, TreeList.nil)
+    print(t2)
+    print(Tree.val(t1))
+    print(simplify(Tree.val(t1)))
 
     s = Solver()
-    t1, t2, t3 = Consts('t1 t2 t3', TreeList)
     s.add(Distinct(t1, t2, t3))
-    s.add(t1 != t2)
-    # solve(Distinct(t1, t2, t3))
 
     print(s.check())
     print(s.model())
@@ -48,19 +47,34 @@ def sorting_with_datatypes():
 
     # trace ::= new (int, int, int, int)
     Trace = Datatype("Trace")
-    Trace.declare('input', ('one', IntSort()), ('two', IntSort()), ('three', IntSort()), ('four', IntSort()))
+    Trace.declare('trace', ('one', IntSort()), ('two', IntSort()), ('three', IntSort()), ('four', IntSort()))
     Trace = Trace.create()
 
     # Suppose we have two input facts i1 and i2
     i1, i2 = Consts('i1 i2', Input)
     t1, t2 = Consts('t1 t2', Trace)
 
-    s = Solver()
-    s.add(Distinct(i1, i2))
-    s.add(Distinct(t1, t2))
+    # Define some variables of int type to be used in datatypes. 
+    one, two, three, four = Ints("one two three four")
 
-    # How to define constraints between instances of datatypes.
+    # input(one, 2, three, 4) and trace(1, two, 3, four)
+    i3 = Input.input(one, 2, three, 4)
+    t3 = Trace.trace(1, two, 3, four) 
+
+    s = Solver()
+    s.add(Distinct(i1, i2, i3))
+    s.add(Distinct(t1, t2, t3))
+    s.add((one > two + 100))
 
     print(s.check())
-    model = s.model()
-    print(model)
+    if s.check() == sat:
+        model = s.model()
+        print(model)
+
+def test_recursive_datatype():
+    
+    s = Solver()
+
+test_simplify()
+test_treelist()
+sorting_with_datatypes()
