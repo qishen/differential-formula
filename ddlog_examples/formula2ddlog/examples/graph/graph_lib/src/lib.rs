7e1ff85b5df7adc20a81f23f9781b21476858b4a
Qishen Zhang <qishen23@gmail.com>
2021-07-29 22:14:12 -0500

    Update benchmark to output results that can be used directly in latex

---
+++ ddlog_examples/formula2ddlog/examples/graph/graph_lib/src/lib.rs
@@ -0,0 +1,209 @@
+use std::collections::BTreeSet;
+use rand::prelude::StdRng;
+use rand::{Rng, SeedableRng};
+
+use graph_ddlog::Relations;
+use graph_ddlog::relid2name;
+use graph_ddlog::typedefs::*;
+
+use differential_datalog::{DDlog, DDlogDynamic}; 
+use differential_datalog::program::config::{Config, ProfilingConfig};
+use differential_datalog::api::HDDlog;
+use differential_datalog::DeltaMap; // A trait representing the changes resulting from a given update.
+use differential_datalog::ddval::DDValue; // A generic DLog value type
+use differential_datalog::ddval::DDValConvert; //Another helper trair
+use differential_datalog::program::RelId; // Numeric relations id
+use differential_datalog::program::Update; // A type representing updates to the database
+
+pub struct Graph {
+    // The elements must be ordered in nodes and edges
+    node_set: BTreeSet<usize>,
+    edge_set: BTreeSet<(usize, usize)>,
+    seed: u64,
+}
+
+impl Graph {
+    pub fn new_empty() -> Self {
+        Self { 
+            node_set: BTreeSet::new(), 
+            edge_set: BTreeSet::new(),
+            seed: 0
+        }
+    }
+
+    pub fn nodes(&self) -> Vec<usize> {
+        self.node_set.clone().into_iter().collect()
+    }
+
+    pub fn edges(&self) -> Vec<(usize, usize)> {
+        self.edge_set.clone().into_iter().collect()
+    }
+
+    pub fn add_node(&mut self, node: usize) -> bool {
+        if self.node_set.contains(&node) {
+            return false;
+        } else {
+            self.node_set.insert(node);
+            return true;
+        }
+    }
+
+    pub fn add_edge(&mut self, edge: (usize, usize)) -> bool {
+        if self.edge_set.contains(&edge) {
+            return false;
+        } else {
+            let (src, dst) = edge;
+            if !self.node_set.contains(&src) { self.add_node(src); }
+            if !self.node_set.contains(&dst) { self.add_node(dst); }
+            self.edge_set.insert(edge);
+            return true;
+        }
+    }
+
+    pub fn add_random_edge(&mut self) -> (usize, usize) {
+        let mut rng = StdRng::seed_from_u64(self.seed); 
+        // Increment the seed so it won't generate the same graph in the next round
+        self.seed += 1;
+        let num1 = rng.gen_range(0..self.node_set.len());
+        let num2 = rng.gen_range(0..self.node_set.len());
+        let mut edge = (num1, num2);
+        while self.contains_edge(edge) {
+            let num1 = rng.gen_range(0..self.node_set.len());
+            let num2 = rng.gen_range(0..self.node_set.len());
+            edge = (num1, num2);
+        };
+        edge
+    }
+
+    pub fn contains_node(&self, node: usize) -> bool {
+        self.node_set.contains(&node)
+    }
+
+    pub fn contains_edge(&self, edge: (usize, usize)) -> bool {
+        self.edge_set.contains(&edge)
+    }
+
+    pub fn gen_random_graph(nodes_num: usize, edges_num: usize) -> Graph {
+        let mut graph = Graph::new_empty();
+        for i in 0 .. nodes_num {
+            graph.add_node(i);
+        }
+
+        let mut rng1: StdRng = SeedableRng::seed_from_u64(graph.seed); 
+        // Increment the seed so it won't generate the same graph in the next round
+        graph.seed += 1;
+        let mut count = 0;
+        while count < edges_num {
+            let num1 = rng1.gen_range(0..nodes_num);
+            let num2 = rng1.gen_range(0..nodes_num);
+            let edge = (num1, num2);
+            // Check if a duplicate edge is generated, if so disgard and generate a new edge 
+            if !graph.edge_set.contains(&edge) {
+                graph.add_edge(edge);
+                count += 1;
+            }
+        }
+        graph
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum UpdateKind {
+    Insert,
+    Delete,
+    DeleteByKey,
+    // Modify
+}
+
+pub struct DDLogGraph{
+    debug: bool,
+    hddlog: HDDlog,
+}
+
+impl DDLogGraph {
+    pub fn new(debug: bool)  -> Result<DDLogGraph, String> {
+        let config = Config::new()
+            .with_timely_workers(1)
+            .with_profiling_config(ProfilingConfig::SelfProfiling);
+        // let (hddlog, init_state) = formula2ddlog_ddlog::run_with_config(config, false); 
+        let (hddlog, init_state) = graph_ddlog::run_with_config(config, false)?;
+        Self::dump_delta(&init_state);
+        return Ok(Self { debug, hddlog });
+    }
+
+    pub fn flush_updates(&mut self, updates: Vec<Update<DDValue>>) -> Result<DeltaMap<DDValue>, String> {
+        self.hddlog.transaction_start()?;
+        self.hddlog.apply_updates(&mut updates.into_iter())?;
+        let delta = self.hddlog.transaction_commit_dump_changes()?;
+        return Ok(delta); 
+    }
+
+    pub fn gen_node_updates(&mut self, nodes: Vec<usize>, kind: UpdateKind) -> Vec<Update<DDValue>> {
+        let updates = nodes.into_iter().map(|num| {
+            let relid = Relations::NodeInput as RelId;
+            let v = Node { name: Graph_AUTOTYPE0::Graph_AUTOTYPE0_usize{usize_field: num as u64} }.into_ddvalue();
+            match kind {
+                UpdateKind::Insert => Update::Insert { relid, v },
+                UpdateKind::Delete => Update::DeleteValue { relid, v },
+                UpdateKind::DeleteByKey => Update::DeleteKey { relid, k: v },
+            }
+        }).collect::<Vec<_>>();
+
+        updates
+    } 
+
+    pub fn gen_edge_updates(&mut self, edges: Vec<(usize, usize)>, kind: UpdateKind) -> Vec<Update<DDValue>> {
+        let updates = edges.into_iter().map(|(src, dst)| {
+            let relid = Relations::EdgeInput as RelId;
+            let v = Edge { 
+                src: Node { name: Graph_AUTOTYPE0::Graph_AUTOTYPE0_usize{usize_field: src as u64} }, 
+                dst: Node { name: Graph_AUTOTYPE0::Graph_AUTOTYPE0_usize{usize_field: dst as u64} } 
+            }.into_ddvalue();
+            match kind {
+                UpdateKind::Insert => Update::Insert { relid, v },
+                UpdateKind::Delete => Update::DeleteValue { relid, v },
+                UpdateKind::DeleteByKey => Update::DeleteKey { relid, k: v },
+            }
+        }).collect::<Vec<_>>();
+
+        updates
+    } 
+
+    pub fn init_graph(&mut self, nodes: Vec<usize>, edges: Vec<(usize, usize)>) -> (usize, usize, u128) {
+        let (nodes_num, edges_num, time_elapsed) = self.update_graph(nodes, edges, UpdateKind::Insert);
+        (nodes_num, edges_num, time_elapsed)
+    }
+
+    pub fn update_graph(&mut self, nodes: Vec<usize>, edges: Vec<(usize, usize)>, kind: UpdateKind) -> (usize, usize, u128) {
+        let nodes_num = nodes.len();
+        let edges_num = edges.len();
+        let mut updates = vec![];
+        let node_updates = self.gen_node_updates(nodes, kind);
+        let edge_updates = self.gen_edge_updates(edges, kind);
+        updates.extend(node_updates);
+        updates.extend(edge_updates);
+
+        let timer = std::time::Instant::now();
+        let delta = self.flush_updates(updates).unwrap();
+        let time_elapsed = timer.elapsed().as_millis();
+
+        if self.debug { 
+            DDLogGraph::dump_delta(&delta); 
+        }
+
+        (nodes_num, edges_num, time_elapsed)
+    } 
+
+    pub fn dump_delta(delta: &DeltaMap<DDValue>) {
+        for (rel, changes) in delta.iter() {
+            println!("Changes to relation {}", relid2name(*rel).unwrap());
+            for (val, weight) in changes.iter() {
+                println!("{} {:+}", val, weight);
+            }
+        }
+    }
+
+    pub fn stop(&mut self){
+        self.hddlog.stop().unwrap();
+    }
+}
--- ddlog_examples/formula2ddlog/examples/graph/graph_lib/src/main.rs
+++ ddlog_examples/formula2ddlog/examples/graph/graph_lib/src/main.rs
@@ -1,161 +1,64 @@
-use std::collections::HashSet;
-use rand::{Rng, SeedableRng, StdRng};
+use graph_lib::*;
 
-use graph_ddlog::Relations;
-use graph_ddlog::relid2name;
-// import all types defined by the datalog program itself
-use graph_ddlog::typedefs::*;
-
-// Trait that must be implemented by an instance of a DDlog program. 
-// Type that represents a set of changes to DDlog relations.
-// Returned by `DDlog::transaction_commit_dump_changes()`.
-use differential_datalog::{DDlog, DDlogDynamic}; 
-use differential_datalog::program::config::{Config, ProfilingConfig};
-use differential_datalog::api::HDDlog;
-use differential_datalog::DeltaMap; // A trait representing the changes resulting from a given update.
-use differential_datalog::ddval::DDValue; // A generic DLog value type
-use differential_datalog::ddval::DDValConvert; //Another helper trair
-use differential_datalog::program::RelId; // Numeric relations id
-use differential_datalog::program::Update; // A type representing updates to the database
-// use differential_datalog::record::{FromRecord, IntoRecord}; // A type representing individual facts
-
-pub struct DDLogGraph{
-    hddlog: HDDlog,
-}
-
-impl DDLogGraph {
-    pub fn new()  -> Result<DDLogGraph, String> {
-        let config = Config::new()
-            .with_timely_workers(1)
-            .with_profiling_config(ProfilingConfig::SelfProfiling);
-        // let (hddlog, init_state) = formula2ddlog_ddlog::run_with_config(config, false); 
-        let (hddlog, init_state) = graph_ddlog::run_with_config(config, false)?;
-        Self::dump_delta(&init_state);
-        return Ok(Self{hddlog});
-    }
-
-    pub fn flush_updates(&mut self, updates: Vec<Update<DDValue>>) -> Result<DeltaMap<DDValue>, String> {
-        self.hddlog.transaction_start()?;
-        self.hddlog.apply_updates(&mut updates.into_iter())?;
-        let delta = self.hddlog.transaction_commit_dump_changes()?;
-        return Ok(delta); 
-    }
-
-    pub fn create_nodes(&mut self, users: Vec<u64>) -> Vec<Update<DDValue>> {
-        let updates = users.into_iter().map(|num| {
-            Update::Insert {
-                relid: Relations::NodeInput as RelId,
-                v: Node { name: Graph_AUTOTYPE0::Graph_AUTOTYPE0_usize{usize_field: num} }.into_ddvalue(),
-            }
-        }).collect::<Vec<_>>();
-
-        updates
-    } 
-
-    pub fn create_edges(&mut self, users: Vec<(u64, u64)>) -> Vec<Update<DDValue>> {
-        let updates = users.into_iter().map(|(src, dst)| {
-            Update::Insert {
-                relid: Relations::EdgeInput as RelId,
-                v: Edge { 
-                    src: Node { name: Graph_AUTOTYPE0::Graph_AUTOTYPE0_usize{usize_field: src} }, 
-                    dst: Node { name: Graph_AUTOTYPE0::Graph_AUTOTYPE0_usize{usize_field: dst} } 
-                }.into_ddvalue(),
-            }
-        }).collect::<Vec<_>>();
-
-        updates
-    } 
-
-    pub fn dump_delta(delta: &DeltaMap<DDValue>) {
-        for (rel, changes) in delta.iter() {
-            println!("Changes to relation {}", relid2name(*rel).unwrap());
-            for (val, weight) in changes.iter() {
-                println!("{} {:+}", val, weight);
-            }
-        }
-    }
-
-    pub fn stop(&mut self){
-        self.hddlog.stop().unwrap();
-    }
-}
-
-
-fn generate_graph(nodes_num: usize, edges_num: usize, seed: &[usize]) -> (Vec<u64>, Vec<(u64, u64)>) {
-    let mut rng1: StdRng = SeedableRng::from_seed(seed);    // rng for edge additions
-    let mut node_set = HashSet::new();
-    let mut edge_set = HashSet::new();
-    let mut i = 0;
-    while i < edges_num {
-        let num1 = rng1.gen_range(0, nodes_num);
-        let num2 = rng1.gen_range(0, nodes_num);
-        node_set.insert(num1 as u64);
-        node_set.insert(num2 as u64);
-        let edge = (num1 as u64, num2 as u64);
-        // In case duplicates are generated
-        if !edge_set.contains(&edge) {
-            edge_set.insert(edge);
-            i += 1;
-        }
-    }
-    let nodes: Vec<u64> = node_set.into_iter().collect();
-    let edges: Vec<(u64, u64)> = edge_set.into_iter().collect();
-    (nodes, edges)
-}
-
-
+// Usage:
+// cargo run --release -- <NODE> <EDGE> <TARGET> debug
 fn main() {
-    println!("Print out arguments: {:?}", std::env::args());
-
-    let nodes_num: usize = std::env::args().nth(1).unwrap_or("100".to_string()).parse().unwrap_or(100);
-    let edges_num: usize = std::env::args().nth(2).unwrap_or("20".to_string()).parse().unwrap_or(20);
+    // println!("Print out arguments: {:?}", std::env::args());
+    let nodes_num: usize = std::env::args().nth(1).unwrap_or("0".to_string()).parse().unwrap_or(0);
+    let edges_num: usize = std::env::args().nth(2).unwrap_or("0".to_string()).parse().unwrap_or(0);
     let target: String = std::env::args().nth(3).unwrap_or("ddlog".to_string());
+    // By default, do not print out delta.
     let debug = std::env::args().nth(4).unwrap_or("nodebug".to_string()) == "debug";
 
-    let seed: &[_] = &[1, 2, 3, 4];
-    let (nodes, edges) = generate_graph(nodes_num, edges_num, seed);
+    let graph = Graph::gen_random_graph(nodes_num, edges_num);
+    let nodes = graph.nodes();
+    let edges = graph.edges();
+
+    // Print out the header
+    println!("nodes,edges,init,add,del");
 
     if target == "formula" {
-        for (src, dst) in edges.iter() { println!("Edge(Node({}), Node({})).", src, dst); }
-    } else {
-        if debug {
-            for edge in edges.iter() { println!("Initial input: {:?}", edge); }
+        for (src, dst) in edges.iter() { 
+            println!("Edge(Node({}), Node({})).", src, dst); 
         }
-
-        let mut timer = std::time::Instant::now();
-        let mut bse = DDLogGraph::new().unwrap();
-
-        let mut updates = vec![];
-        let node_updates = bse.create_nodes(nodes);
-        let edge_updates = bse.create_edges(edges);
-        updates.extend(node_updates);
-        updates.extend(edge_updates);
-
-        // println!("Loading Time: {} seconds", timer.elapsed().as_secs());
-        // timer = std::time::Instant::now();
-
-        let mut delta = bse.flush_updates(updates).unwrap();
-        if debug {
-            DDLogGraph::dump_delta(&delta);
+    } else if target == "formula_bench" {
+        for i in 0 .. 30 {
+            let nodes_num = 2000;
+            let edges_num = 100 * (i + 1);
+            let graph = Graph::gen_random_graph(nodes_num, edges_num);
+            let edges = graph.edges();
+            for (src, dst) in edges.iter() { 
+                println!("Edge(Node({}), Node({})).", src, dst); 
+            }
         }
+    } else if target == "ddlog" {
+        let mut bse = DDLogGraph::new(debug).unwrap();
+        let (_,_,t) = bse.init_graph(nodes, edges);
+        println!("Nodes: {}, Edges: {}, Running time: {} milli seconds", nodes_num, edges_num, t);
+    } else if target == "ddlog_bench" {
+        for i in 0 .. 30 {
+            let nodes_num = 2000;
+            let edges_num = 100 * (i + 1);
+            let mut graph = Graph::gen_random_graph(nodes_num, edges_num);
+            let nodes = graph.nodes();
+            let edges = graph.edges();
+            let mut bse = DDLogGraph::new(debug).unwrap();
+            let (_,_, t1) = bse.init_graph(nodes, edges);
+            
+            // Create more random edges to test incremental update 
+            let mut new_edges = vec![];
+            for _ in 0 .. 5 {
+                let new_edge = graph.add_random_edge();
+                new_edges.push(new_edge);
+            }
 
-        println!("Graph has {} nodes and {} edges. Running Time: {} milliseconds", 
-            nodes_num, edges_num, timer.elapsed().as_millis());
+            // Add new edges that have not been added to current graph before
+            let (_,_,t2) = bse.update_graph(vec![], new_edges.clone(), UpdateKind::Insert);
 
-        let new_edges_num = edges_num / 10;
-        let (new_nodes, new_edges) = generate_graph(nodes_num, new_edges_num / 10, &[4,3,2,1]);
-        let mut new_updates = vec![]; 
-        let new_node_updates = bse.create_nodes(new_nodes);
-        let new_edge_updates = bse.create_edges(new_edges);
-        new_updates.extend(new_node_updates);
-        new_updates.extend(new_edge_updates);
+            // Delete the new added edge from graph
+            let (_,_,t3) = bse.update_graph(vec![], new_edges, UpdateKind::Delete);
 
-        timer = std::time::Instant::now();
-        delta = bse.flush_updates(new_updates).unwrap();
-        if debug {
-            DDLogGraph::dump_delta(&delta);
+            println!("{},{},{},{},{}", nodes_num, edges_num, t1, t2, t3);
         }
-
-        println!("Add {} new edges. Running Time: {} milliseconds", new_edges_num, timer.elapsed().as_millis());
     }
 }
