2ff3f4861bbf9879be9803b7785fbed367c34a24
Qishen Zhang <qishen23@gmail.com>
2021-07-31 22:34:26 -0500

    Update benchmarking with more automation and fixes
    - Create a better bash script to automate benchmarking
    
    - Change incremental updates benchmark to delete first then insert the
      same records

--- ddlog_examples/formula2ddlog/examples/graph/graph_lib/src/main.rs
+++ ddlog_examples/formula2ddlog/examples/graph/graph_lib/src/main.rs
@@ -2,6 +2,18 @@ use std::fs;
 use std::path::Path;
 use graph_lib::*;
 
+fn gen_formula_program(domain_str: String, nodes_num: usize, edges_num: usize) -> String {
+    let mut program_model_str = "".to_string();
+    let graph = Graph::gen_random_graph(nodes_num, edges_num);
+    let edges = graph.edges();
+    for (src, dst) in edges.iter() { 
+        let term_str = format!("Edge(Node({}), Node({})).\n", src, dst); 
+        program_model_str += &term_str[..];
+    }
+    let program_str = format!("{}\n model m of Graph {{\n {} \n}}", domain_str, program_model_str);
+    return program_str;
+}
+
 // Usage:
 // cargo run --release -- <NODE> <EDGE> <TARGET> debug
 fn main() {
@@ -12,61 +24,55 @@ fn main() {
     // By default, do not print out delta.
     let debug = std::env::args().nth(4).unwrap_or("nodebug".to_string()) == "debug";
 
-    let graph = Graph::gen_random_graph(nodes_num, edges_num);
-    let nodes = graph.nodes();
-    let edges = graph.edges();
-
     // Print out the header
-    println!("nodes,edges,init,add,del");
+    println!("nodes,edges,init,del,add");
 
     if target == "formula" {
-        for (src, dst) in edges.iter() { 
-            println!("Edge(Node({}), Node({})).", src, dst); 
-        }
+        let path = Path::new("../graph_template.4ml");
+        let domain_str = fs::read_to_string(path).unwrap();
+        let program_str = gen_formula_program(domain_str, nodes_num, edges_num);
+        println!("{}", program_str);
+        let write_path = format!("../files/graph_n{}_e{:04}.4ml", nodes_num, edges_num);
+        fs::write(write_path, program_str).unwrap();
     } else if target == "formula_bench" {
         let path = Path::new("../graph_template.4ml");
         let domain_str = fs::read_to_string(path).unwrap();
-        for i in 0 .. 30 {
-            let mut program_model_str = "".to_string();
-            let nodes_num = 2000;
-            let edges_num = 100 * (i + 1);
-            let graph = Graph::gen_random_graph(nodes_num, edges_num);
-            let edges = graph.edges();
-            for (src, dst) in edges.iter() { 
-                let term_str = format!("Edge(Node({}), Node({})).\n", src, dst); 
-                program_model_str += &term_str[..];
-            }
-            let program_str = format!("{}\n model m of Graph {{\n {} \n}}", domain_str, program_model_str);
+        for i in 0 .. 50 {
+            let edge_interval = edges_num;
+            let edges_num = edge_interval * (i + 1);
+            let program_str = gen_formula_program(domain_str.clone(), nodes_num, edges_num);
             println!("{}", program_str);
-            let write_path = format!("../files/graph_n{}_e{}.4ml", nodes_num, edges_num);
+            let write_path = format!("../files/graph_n{}_e{:04}.4ml", nodes_num, edges_num);
             fs::write(write_path, program_str).unwrap();
         }
     } else if target == "ddlog" {
+        let graph = Graph::gen_random_graph(nodes_num, edges_num);
+        let nodes = graph.nodes();
+        let edges = graph.edges();
         let mut bse = DDLogGraph::new(debug).unwrap();
         let (_,_,t) = bse.init_graph(nodes, edges);
         println!("Nodes: {}, Edges: {}, Running time: {} milli seconds", nodes_num, edges_num, t);
     } else if target == "ddlog_bench" {
-        for i in 0 .. 30 {
-            let nodes_num = 2000;
-            let edges_num = 100 * (i + 1);
-            let mut graph = Graph::gen_random_graph(nodes_num, edges_num);
+        for i in 0 .. 50 {
+            let edge_interval = edges_num;
+            let edges_num = edge_interval * (i + 1);
+            let graph = Graph::gen_random_graph(nodes_num, edges_num);
             let nodes = graph.nodes();
             let edges = graph.edges();
             let mut bse = DDLogGraph::new(debug).unwrap();
             let (_,_, t1) = bse.init_graph(nodes, edges);
             
-            // Create more random edges to test incremental update 
-            let mut new_edges = vec![];
-            for _ in 0 .. 5 {
-                let new_edge = graph.add_random_edge();
-                new_edges.push(new_edge);
-            }
+            // For specific graph algorithm like transitive closure, adding one edge to the graph
+            // could potentially double the running time so we delete some edges first and then 
+            // add them back to show the benefits of incremental updates 
+            let mut one_percent_edges = graph.edges();
+            one_percent_edges.truncate(edges_num / 100);
+            
+            // Delete some existing edges
+            let (_,_,t2) = bse.update_graph(vec![], one_percent_edges.clone(), UpdateKind::Delete);
 
-            // Add new edges that have not been added to current graph before
-            let (_,_,t2) = bse.update_graph(vec![], new_edges.clone(), UpdateKind::Insert);
-
-            // Delete the new added edge from graph
-            let (_,_,t3) = bse.update_graph(vec![], new_edges, UpdateKind::Delete);
+            // Add the deleted edges back again
+            let (_,_,t3) = bse.update_graph(vec![], one_percent_edges, UpdateKind::Insert);
 
             println!("{},{},{},{},{}", nodes_num, edges_num, t1, t2, t3);
         }
--- ddlog_examples/formula2ddlog/run.sh
+++ ddlog_examples/formula2ddlog/run.sh
@@ -1,12 +1,80 @@
 #!/bin/bash
 
-# Build formula2ddlog runtime from formula2ddlog.dl
+echo '
+Usage:
+run.sh <NODE=2000> <EDGE=100> <TARGET=ddlog>
+if the target is `ddlog` or `formula`, <NODE> and <EDGE> are the attributes of the auto-generated random graph.
+if the target is for benchmark as `*_bench`, <NODE> is the same attribute for the auto-generated random graph 
+but <EDGE> is number of both initial edges and the range interval.
+'
+NODE_NUM=${1-2000}
+EDGE_NUM=${2-100}
+TARGET=${3-ddlog}
+echo "Print out bash script arguments: Nodes=$NODE_NUM, Edges=$EDGE_NUM, Target=$TARGET"
 
-# Use `formula2ddlog` to transform graph.4ml into graph.dl
-formula2ddlog_lib/target/release/formula2ddlog_lib examples/graph/graph.4ml > examples/graph/graph.dl &&
-# The std lib is missing so we have to specify the path.
-ddlog -i examples/graph/graph.dl -L ~/differential-datalog/lib &&
-(cd examples/graph/graph_lib && cargo build --release && cargo run --release -- 200 2000) 
+if [[ $TARGET = ddlog* ]]; then
+	echo "Compiling `formula2ddlog` transformation into Rust runtime" 
+	# Build formula2ddlog Rust runtime from formula2ddlog.dl and compile it all the way to binary
+	ddlog -i formula2ddlog.dl -L $HOME/differential-datalog/lib && 
+	(cd formula2ddlog_lib && cargo build --release)
 
-# Uncomment this line to update records from ddlog commandline language
-# && (cd examples/graph/graph_ddlog && cargo build --release && target/release/graph_cli < ../graph.dat) 
+	echo "Generating DDLog program `graph.dl` from FORMULA program `graph.4ml`"
+	# Use `formula2ddlog` to transform graph.4ml into graph.dl
+	formula2ddlog_lib/target/release/formula2ddlog_lib examples/graph/graph.4ml > examples/graph/graph.dl &&
+
+	echo "Compiling DDLog program `graph.dl` to Rust runtime"
+	# If the std lib is missing we have to specify the path when generating Rust runtime for domain
+	ddlog -i examples/graph/graph.dl -L $HOME/differential-datalog/lib
+
+	pushd examples/graph/graph_lib > /dev/null
+	if [[ $TARGET = "ddlog_bench" ]]; then
+		echo "Start DDLog benchmark starting from $NODE_NUM nodes and $EDGE_NUM edges"
+		cargo run --release -- $NODE_NUM $EDGE_NUM ddlog_bench
+	elif [[ $TARGET = "ddlog_manual" ]]; then
+		echo "Running DDLog graph computation with inputs from `graph.dat` incrementally"
+		pushd ../graph_ddlog > /dev/null
+		# Update records from ddlog commandline language
+		cargo build --release && target/release/graph_cli < ../graph.dat
+		popd
+	else 
+		echo "Running DDLog graph computation once on a random graph with $NODE_NUM nodes and $EDGE_NUM edges"
+		cargo run --release -- $NODE_NUM $EDGE_NUM ddlog
+	fi
+	popd > /dev/null
+
+elif [[ $TARGET = formula* ]]; then
+
+	# Copy noninteractive executable from archive to the folder that contains all FORMULA files for testing
+	cp -r ../../archive/executable_noninteractive/* examples/graph/files/
+
+	pushd examples/graph/files/ > /dev/null
+	if [[ $TARGET = "formula_bench" ]]; then
+		echo "Clean folder and generate .4ml files for benchmark starting from $NODE_NUM nodes and $EDGE_NUM edges"
+		rm *.4ml
+		pushd ../graph_lib > /dev/null
+		cargo run --release -- $NODE_NUM $EDGE_NUM formula_bench
+		popd
+		for FILE in $(find . -maxdepth 1 -type f -name \*.4ml | sort); do 
+			load_start=`date +%s%3N`
+			dotnet CommandLine.dll "load $FILE" > /dev/null
+			load_end=`date +%s%3N`
+			load_time=`expr $load_end - $load_start`
+
+			start=`date +%s%3N`
+			dotnet CommandLine.dll "load $FILE | qr m Path(a, b)" > /dev/null
+			end=`date +%s%3N`
+			execution_time=`expr $end - $start`
+			# The real execution time needs to minus the loading time
+			echo "Execution time of $FILE is `expr $execution_time - $load_time` milliseconds"
+		done
+	else 
+		echo "Clean folder and generate one .4ml file for testing"
+		rm *.4ml
+		pushd ../graph_lib > /dev/null
+		cargo run --release -- $NODE_NUM $EDGE_NUM formula
+		popd
+		dotnet CommandLine.dll "load graph_n${NODE_NUM}_e${EDGE_NUM}.4ml | qr m Path(a, b)"
+	fi
+	popd > /dev/null
+fi
+
